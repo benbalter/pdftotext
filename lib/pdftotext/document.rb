33831c2151f4bc393469309382c2bce272f03a4e
AWang <angeladdpiano@gmail.com>
2018-02-27 16:41:07 -0500

    my code

--- lib/pdftotext/document.rb
+++ lib/pdftotext/document.rb
@@ -9,7 +9,7 @@ module Pdftotext
     def text(options={})
       Tempfile.open(['pdftotext', '.txt']) do |file|
         Pdftotext.cli.run_command path, file.path, options
-        file.read.force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+        file.read #Angela's code
       end
     end
 
